492fa3279c26553fa5fb9c67d01444df478f09b3
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-10 18:43:18 +0900

    feat: learn basic oop - interface

---
+++ oop/animal-interface/index.php
@@ -0,0 +1,22 @@
+<?php
+spl_autoload_register(function($className){
+    include 'src/'.$className . '.php';
+});
+
+header('Content-Type:text/plain', true);
+
+$billy = new Dog;
+$santa = new Lion;
+$loko = new Rabbit;
+
+$billy->chase($loko);
+$billy->kill($loko);
+
+$santa->chase($billy);
+$santa->kill($billy);
+
+$loko->chaseBy($billy);
+$loko->killedBy($billy);
+
+
+
