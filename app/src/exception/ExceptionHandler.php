483b85dfcb0193c5ebffdbd6e78e0d41d9af3f40
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-12 08:52:23 +0900

    feat: core exception handler

---
+++ app/src/exception/ExceptionHandler.php
@@ -0,0 +1,26 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace App\Exception;
+use App\Helpers\App;
+use Throwable;
+
+class ExceptionHandler 
+{
+    public function handle(Throwable $exception): void
+    {
+        $application = new App();
+        if($application->isDebugMode()){
+            var_dump($exception);
+        }else{
+            echo "This should not have happened, please try again";
+        }
+        exit;
+    }
+
+
+
+}
+
+?>
