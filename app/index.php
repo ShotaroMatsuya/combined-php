0d468d211976f117235b2af881a26875cbe6f84f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-12 08:50:06 +0900

    feat: create helper for config related with app specifically

--- app/index.php
+++ app/index.php
@@ -9,4 +9,19 @@ header('Content-Type:text/plain', true);
 $config = \App\Helpers\Config::get('appasd');
 var_dump($config);
 
+
+// App check
+
+$application = new \App\Helpers\App();
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+
+if ($application->isRunningFromConsole()){
+    echo 'from console';
+}else {
+    echo 'from browser';
+}
+
 ?>
