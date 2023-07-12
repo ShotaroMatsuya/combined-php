0d4fa21fc4925694e16163e952ce49d2a0662c49
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-12 18:54:26 +0900

    tests: check generating log file

--- app/index.php
+++ app/index.php
@@ -7,27 +7,13 @@ require_once __DIR__ . '/src/exception/exception.php';
 
 header('Content-Type:text/plain', true);
 
-// Error Check(set_error_handler)
-$db = new mysqli('sdfdsfsd', 'root', '', 'bug');
-exit;
+// Logger Check
 
-// Config check(set_exception_handler)
-$config = \App\Helpers\Config::get('appasd');
-var_dump($config);
+$logger = new \App\Logger\Logger();
+$logger->log(
+    \App\Logger\LogLevel::EMERGENCY, 'There is an emergency',
+    ['exception' => 'exception occurred']
+);
 
-
-// App check
-
-$application = new \App\Helpers\App();
-echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
-echo $application->getLogPath() . PHP_EOL;
-echo $application->getEnvironment() . PHP_EOL;
-echo $application->isDebugMode() . PHP_EOL;
-
-if ($application->isRunningFromConsole()){
-    echo 'from console';
-}else {
-    echo 'from browser';
-}
-
+$logger->info('User account created successfully', ['id' => 5]);
 ?>
