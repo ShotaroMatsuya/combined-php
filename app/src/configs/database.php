4aeee841c7db65cec6e6e47e47ffc501d4ee38ec
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-12 22:23:23 +0900

    chore: configure db credentials

--- app/src/configs/database.php
+++ app/src/configs/database.php
@@ -1,7 +1,21 @@
 <?php 
 
 return [
-
+    'pdo' => [
+        'driver' => 'mysql',
+        'host' => 'mysql',
+        'db_name' => 'bug_app',
+        'db_username' => 'bug_app_user',
+        'db_user_password' => 'secret',
+        'default_fetch' => PDO::FETCH_OBJ
+    ],
+    'mysqli' => [
+        'host' => 'mysql',
+        'db_name' => 'bug_app',
+        'db_username' => 'bug_app_user',
+        'db_user_password' => 'secret',
+        'default_fetch' => MYSQLI_ASSOC
+    ]
     
 ];
 
