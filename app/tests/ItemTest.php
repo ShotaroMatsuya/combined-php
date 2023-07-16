4f6f68840c3b44b56a989cbffd96054b461e6d18
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-17 02:23:41 +0900

    tests: private method with argument using reflection

--- app/tests/ItemTest.php
+++ app/tests/ItemTest.php
@@ -34,4 +34,17 @@ class ItemTest extends TestCase
         $this->assertIsString($result);
     }
     
+    public function testPrefixedTokenStartsWithPrefix()
+    {
+        $item = new Item;
+
+        $reflector = new ReflectionClass(Item::class);
+
+        $method = $reflector->getMethod('getPrefixedToken');
+        $method->setAccessible(true);        
+
+        $result = $method->invokeArgs($item, ['example']);
+
+        $this->assertStringStartsWith('example', $result);        
+    }    
 }
